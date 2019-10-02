5a63e4c2c946408b2ea13c9d24a3a016579a499c
Yusuf Abdulkarim <lambda@Yusufs-MacBook-Pro.local>
2019-10-02 15:26:35 +0100

    add Player Class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+
+    def __init__(self, name, current_room):
+        self.current_room = current_room
+
+    def getCurrentRoom(self):
+        return self.current_room
+
+    def getName(self):
+        return self.name
