bff35177309674931ad5424e44650f5342432f42
Yusuf Abdulkarim <lambda@Yusufs-MacBook-Pro.local>
2019-10-04 06:10:44 +0100

    add on_take and on_drop method to item class

--- src/item.py
+++ src/item.py
@@ -4,5 +4,11 @@ class Item:
         self.name = name
         self.description = description
 
+    def on_take(self):
+        print(f"You have picked up {self.name.capitalize()}")
+    
+    def on_drop(self):
+        print(f"You just dropped {self.name.capitalize()}")
+
     def __str__(self):
         return self.name
